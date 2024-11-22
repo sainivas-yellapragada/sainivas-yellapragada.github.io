b6cf9940515e3376c26cd63d1d0c54c984617527
sainivas-yellapragada <sainivasyellapragada@gmail.com>
2024-11-22 21:03:23 +0530

    added files for portfolio

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Web Developer", "Computer Programmer", "Instagrammer"],
+    typeSpeed: 100,
+    backSpeed: 100, // Corrected from 'backspeed' to 'backSpeed'
+    backDelay: 1000,
+    loop: true,
+  });
