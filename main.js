6d07b02f98d078c56ecb974dd667e43c1b2a51b5
sainivas-yellapragada <sainivasyellapragada@gmail.com>
2024-11-23 09:34:54 +0530

    made some changes

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text", {
-    strings: ["Web Developer", "Computer Programmer", "Instagrammer"],
+    strings: ["Web Developer", "Computer Programmer", "Car Enthusiast"],
     typeSpeed: 100,
     backSpeed: 100, // Corrected from 'backspeed' to 'backSpeed'
     backDelay: 1000,
